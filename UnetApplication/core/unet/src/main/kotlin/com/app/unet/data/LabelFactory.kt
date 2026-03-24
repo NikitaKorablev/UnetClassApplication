2cfc9f6de1b6c2b2cf1a10e912c7b79db09984a3
Никита Кораблев <kit06092003@gmail.com>
2026-03-24 19:03:26 +0300

    add: Фарика моделей

--- UnetApplication/core/unet/src/main/kotlin/com/app/unet/data/LabelFactory.kt
+++ UnetApplication/core/unet/src/main/kotlin/com/app/unet/data/LabelFactory.kt
@@ -1,4 +1,26 @@
 package com.app.unet.data
 
+import com.app.model.PredictedClasses
+import com.app.unet.models.classes.Axon
+import com.app.unet.models.classes.BaseLabel
+import com.app.unet.models.classes.Boundaries
+import com.app.unet.models.classes.Mitochondria
+import com.app.unet.models.classes.MitochondriaBoundaries
+import com.app.unet.models.classes.PSD
+import com.app.unet.models.classes.Vesicles
+
 class LabelFactory {
+    companion object {
+        fun getLabel(className: String, label: Array<FloatArray>): BaseLabel {
+            return when (className) {
+                PredictedClasses.MITOCHONDRIA.className -> Mitochondria(label)
+                PredictedClasses.PSD.className -> PSD(label)
+                PredictedClasses.VESICLES.className -> Vesicles(label)
+                PredictedClasses.AXON.className -> Axon(label)
+                PredictedClasses.BOUNDARIES.className -> Boundaries(label)
+                PredictedClasses.MITOCHONDRIAL_BOUNDARIES.className -> MitochondriaBoundaries(label)
+                else -> throw IllegalArgumentException("Unknown class getClassName: $className")
+            }
+        }
+    }
 }
